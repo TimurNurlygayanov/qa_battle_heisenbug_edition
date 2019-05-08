7bc3ad79c50b205339798781dd50b9ee2fcc7357
Timur Nurlygayanov <t.nurlygaianov@semrush.com>
2019-05-08 16:21:30 +0300

    Added nginx config file

--- update.sh
+++ update.sh
@@ -3,4 +3,8 @@
 rm -rf  /var/www/qabattle/*
 cp templates/* /var/www/qabattle/
 
+rm -rf /etc/nginx/sites-enabled/
+chmod 644 qabattle
+cp qabattle /etc/nginx/sites-enabled/qabattle
+
 systemctl restart nginx
