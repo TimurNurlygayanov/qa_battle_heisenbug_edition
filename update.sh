aa6ab3a2739565c472547752b9b28389fb6d585e
Timur Nurlygayanov <t.nurlygaianov@semrush.com>
2019-05-09 11:49:45 +0300

    Added uwsgi configs

--- update.sh
+++ update.sh
@@ -2,14 +2,26 @@
 
 pip install -r requirements.txt -y
 
+
 rm -rf  /var/www/qabattle/*
 cp -r templates/* /var/www/qabattle/
+
 
 rm -rf /etc/nginx/sites-enabled/*
 chmod 644 qabattle
 cp qabattle /etc/nginx/sites-enabled/qabattle
 
-systemctl restart nginx
+
+rm -rf /qabattle
+mkdir /qabattle
+chmod 664 /qabattle
+touch /qabattle/qabattle.sock
+chmod 664 /qabattle/qabattle.sock
+
 
 rm -rf /etc/init/qabattle.conf
 cp qabattle.conf  /etc/init/qabattle.conf
+
+
+service nginx configtest
+systemctl restart nginx
